3d248940019a39cd9b808d5ca996c97ebd428c73
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-04-30 13:50:57 +0900

    동인 언니 Closure 설명 코드 추가

--- 0430/practice3.js
+++ 0430/practice3.js
@@ -36,3 +36,20 @@ console.log(incByOne()); // 7
 const incByTen = createIncrementer(10, 10);
 console.log(incByTen()); // 20
 console.log(incByTen()); // 30
+
+// const를 사용하는게 일반적이다!!!!
+
+/* 동인 언니의 다른 케이스 예시 코드 */
+function createIncrementer2(step) {
+    const retFunc = (start) => {
+        return start + step;
+    }
+    return retFunc;
+}
+const func1 = createIncrementer2(1);
+const func2 = createIncrementer2(2);
+console.log(func1(2));
+console.log(func2(2));
+
+console.log(typeof (func1(2))); // 출력: number
+console.log(typeof (func2)); // 출력: function
