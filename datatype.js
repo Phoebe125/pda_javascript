db8c52ad6f8d2582b077fc0254893ec71a264656
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-04-30 10:22:31 +0900

    산술 연산자 코드 추가

---
+++ datatype.js
@@ -0,0 +1,25 @@
+console.log(typeof (10));
+
+console.log(typeof (10.1));
+
+console.log(typeof (true));
+
+console.log(typeof ("Hello JS"));
+
+console.log(typeof ({ name: "jihun", "height": 186 }));
+
+let a;
+console.log(typeof (a));
+
+let b = null;
+console.log(typeof (b));
+
+
+// 산술 연산자
+console.log(3*5);
+
+console.log(3*2.1); // 미세한 숫자 차이가 있을 수 있지만, 무시해도 된다.
+
+let c = 0;
+console.log(c++);
+console.log(++c);
