e054ef5243e262e6a5f4fdd484ffac8b1e9acd51
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-04-30 13:28:47 +0900

    closure 실습 먼저 해보기

---
+++ 0430/practice3.js
@@ -0,0 +1,38 @@
+/* 연습 문제 1 */
+function createCounter() {
+    let count = 0;
+    function inner() {
+        count++;
+        return count;
+    }
+    return inner;
+}
+
+const counter1 = createCounter();
+console.log(counter1()); // 1
+console.log(counter1()); // 2
+
+const counter2 = createCounter();
+console.log(counter2()); // 1
+console.log(counter2()); // 2
+console.log(counter1()); // 3
+
+
+/* 연습 문제 2 */
+function createIncrementer(start, step) {
+    let _start = start;
+    let _step = step;
+    function inner() {
+        _start += _step;
+        return _start;
+    }
+    return inner;
+}
+
+const incByOne = createIncrementer(5, 1);
+console.log(incByOne()); // 6
+console.log(incByOne()); // 7
+
+const incByTen = createIncrementer(10, 10);
+console.log(incByTen()); // 20
+console.log(incByTen()); // 30
