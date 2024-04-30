aeaa2689416ec77c2e0f56063ae2f3c314745ec8
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-04-30 11:49:21 +0900

    scope와 closure

--- 0430/function.js
+++ 0430/function.js
@@ -16,7 +16,7 @@ function min2(a, b) {
 // 이런식으로 함수를 정의를 하면, 함수 선언 전에 함수를 호출을 하면, 에러가 발생함
 const max1 = function (a, b) {
     return a > b ? a : b;
-} 
+}
 
 const max2 = (a, b) => {
     return a > b ? a : b;
@@ -31,3 +31,27 @@ console.log(min2(1, 2));
 console.log(max1(5, 7));
 console.log(max2(1, 2));
 console.log(max3(3, 5));
+
+// 3. 함수의 ReturnType에 대해서
+const e = function () {
+    console.log("e");
+}
+
+// 이런 형태의 Function을 Closure라고 한다
+function createGreeting(greetingPrefix) {
+    return function (name) {
+        console.log(greetingPrefix + ", " + name + "!");
+    }
+}
+
+const greetHello = createGreeting("Hello");
+const greetHi = createGreeting("Hi");
+greetHello("Alice"); // 출력: Hello, Alice!
+greetHi("Bob"); // 출력: Hi, Bob!
+console.log(typeof(greetHello)); // 출력: function
+console.log(typeof(greetHi("Bob"))); // 출력: undefined
+
+/** 
+ * Return이 없거나, 비어있는 경우: undefined
+ * 함수를 Return하는 경우: 
+ * **/
