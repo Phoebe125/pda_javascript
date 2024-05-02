58983cb2c190da142ee44594e155e8b3698de13b
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-05-02 11:31:59 +0900

    for문 예제 코드

--- 0502/for-of.js
+++ 0502/for-of.js
@@ -3,6 +3,6 @@ const arr = ['a', 'b', 'c', 'd']
 for (let idx in arr) {
     console.log(idx);
 } 
-for (let item of arr) {
+for (let item of arr) { // 순서가 중요할 때는 for-in 보다 for-of로 
     console.log(item);
 } // 배열도 object의 일종이니 가능
--- 0502/practice2.js
+++ 0502/practice2.js
@@ -14,8 +14,67 @@ for (let i = num; i > 0; i--) {
 // 연습문제 2 (p.118)) 
 let x = [3, 6, 9, 20, -7, 5]
 for (item of x) {
-    console.log(item*10);
+    item *= 10;
+    console.log(item);
 }
 
-let y = {"math": 70, "science": 80, "english": 20}
+let y = { "math": 70, "science": 80, "english": 20 }
+for (key in y) {
+    y[key] += 10;
+    console.log(y[key]);
+}
 
+const num2 = parseInt(prompt("정수를 입력해주세요: "));
+for (let i = 1; i < 10; i++) {
+    console.log(`${num2}*${i} = ${num2 * i}`);
+}
+
+// 연습문제 3 (p.119)
+const words = ["school", "game", "piano", "science", "hotel", "mountian"];
+const result = [];
+for (word of words) {
+    if (word.length >= 6) {
+        result.push(word);
+    }
+}
+console.log(result);
+
+// 구구단
+for (let i = 1; i < 10; i++) {
+    for (let t = 1; t < 10; t++) {
+        console.log(`${i}*${t} = ${i * t}`);
+    }
+}
+
+// 연습문제 4 (p.120)
+for (let i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`${i} 3과 5의 공배수`);
+    }
+    else if (i % 3 == 0) {
+        console.log(`${i} 3의 배수`);
+    }
+    else if (i % 5 == 0) {
+        console.log(`${i} 5의 배수`);
+    }
+    else {
+        console.log(`${i} 그냥 숫자`);
+    }
+}
+
+const num3 = parseInt(prompt("정수를 입력해주세요: "));
+
+for (let i = 1; i < num3 + 1; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(`${i} 3과 5의 공배수`);
+    }
+    else if (i % 3 == 0) {
+        console.log(`${i} 3의 배수`);
+    }
+    else if (i % 5 == 0) {
+        console.log(`${i} 5의 배수`);
+    }
+    else {
+        console.log(`${i} 그냥 숫자`);
+    }
+}
