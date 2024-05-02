2d294334a21ce1943c7b923878e32c39c2e25267
Phoebe125 <80835102+Phoebe125@users.noreply.github.com>
2024-05-02 13:44:38 +0900

    while과 예제 코드

--- 0502/practice3.js
+++ 0502/practice3.js
@@ -1,10 +1,30 @@
-const str1 = prompt("100보다 큰 숫자를 입력해주세요.");
+let num = parseInt(prompt("100보다 큰 숫자를 입력해주세요."));
+while (num < 100) {
+    num = parseInt(prompt("100보다 큰 숫자를 입력해주세요."));
+}
+console.log(num);
 
 function limitCalls(func, n) {
-    // 여기에 코드를 작성하세요.
+    let i = 0;
+    return () => {
+        if (i < n) {
+            func();
+            i++;
+        }
+    }
 }
-const limitedHello = limitCalls(() =>
-    console.log("Hello!"), 2);
+
+function tmpCalls(func, n) {
+    let i = 0;
+    return function () {
+        if (i < n) {
+            func();
+            i++;
+        }
+    }
+}
+const limitedHello = limitCalls(() => console.log("Hello!"), 2);
+
 limitedHello(); // "Hello!"
 limitedHello(); // "Hello!"
 limitedHello(); // 아무 일도 일어나지 않음
---
+++ 0502/while.js
@@ -0,0 +1,12 @@
+let i = 0;
+while (i < 3) {
+    console.log(i);
+    i++;
+}
+
+// do-while
+let t = 0;
+do {
+    console.log(t);
+    t++;
+} while (t < 3)
